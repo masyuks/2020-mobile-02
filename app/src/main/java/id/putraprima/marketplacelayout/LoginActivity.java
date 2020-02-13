96a9bbea8f2cd2a2bb469e5d3a0fdb876b4a694c
masyuks <myukimiftakhurrizqi21@gmail.com>
2020-02-13 10:58:42 +0700

    commit activity main (penambahan arrow), activity login (penambahan arrow & java), activity register (penambahan java) marketplace layout

---
+++ app/src/main/java/id/putraprima/marketplacelayout/LoginActivity.java
@@ -0,0 +1,6 @@
+package id.putraprima.marketplacelayout;
+
+import android.app.Activity;
+
+public class LoginActivity extends Activity {
+}
---
+++ app/src/main/java/id/putraprima/marketplacelayout/RegisterActivity.java
@@ -0,0 +1,6 @@
+package id.putraprima.marketplacelayout;
+
+import android.app.Activity;
+
+public class RegisterActivity extends Activity {
+}
